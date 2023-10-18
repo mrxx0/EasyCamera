3dd99312cbf3df4d1e8a08e5d8447e2f80db05ec
mrxx0 <chcoutur@student.42.fr>
2023-10-18 17:37:25 +0200

    Build base UI for interface
    - Create the 3 main button : gallery access, shutter and swap camera
    This UI will be the base foundation for the rest of the development
    
    Signed-off-by: mrxx0 <chcoutur@student.42.fr>

--- app/src/main/java/com/mrxx0/easycamera/MainActivity.kt
+++ app/src/main/java/com/mrxx0/easycamera/MainActivity.kt
@@ -8,20 +8,33 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.video.ExperimentalVideo
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.sharp.Refresh
+import androidx.compose.material.icons.filled.Brightness1
+import androidx.compose.material.icons.filled.Cached
+import androidx.compose.material.icons.filled.HideImage
 import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -62,26 +75,57 @@ import com.mrxx0.easycamera.ui.theme.EasyCameraTheme
                             controller = controller,
                             modifier = Modifier.fillMaxSize()
                         )
-                        IconButton(
-                            onClick = {
-                                controller.cameraSelector =
-                                    if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
-                                        CameraSelector.DEFAULT_FRONT_CAMERA
-                                    } else
-                                        CameraSelector.DEFAULT_BACK_CAMERA
-
-                            },
+                        Column(
                             modifier = Modifier
+                                .height(200.dp)
                                 .align(Alignment.BottomEnd)
-                                .padding(horizontal = 15.dp, vertical = 40.dp)
+                                .background(Color.Black)
                         ) {
-                            Icon(
-                                imageVector = Icons.Sharp.Refresh,
-                                "Swap camera",
+                            Row (
                                 modifier = Modifier
-                                    .size(64.dp)
-                            )
-
+                                    .fillMaxWidth()
+                                    .background(Color.Black)
+                                    .padding(start = 50.dp, end = 50.dp, top = 32.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ){
+                                OutlinedButton(
+                                    onClick = { },
+                                    modifier= Modifier.size(54.dp),
+                                    shape = CircleShape,
+                                    border= BorderStroke(1.dp, Color.White),
+                                    contentPadding = PaddingValues(8.dp),
+                                    colors = ButtonDefaults.outlinedButtonColors(contentColor =  Color.White)
+                                ) {
+                                    Icon(Icons.Default.HideImage, contentDescription = "Access gallery", tint = Color.White, modifier = Modifier.size(54.dp))
+                                }
+                                OutlinedButton(
+                                    onClick = { },
+                                    modifier= Modifier.size(80.dp),
+                                    shape = CircleShape,
+                                    border= BorderStroke(4.dp, Color.White),
+                                    contentPadding = PaddingValues(4.dp),
+                                    colors = ButtonDefaults.outlinedButtonColors(contentColor =  Color.White)
+                                ) {
+                                    Icon(Icons.Default.Brightness1, contentDescription = "Shutter button", tint = Color.White, modifier = Modifier.size(80.dp))
+                                }
+                                OutlinedButton(
+                                    onClick = {
+                                        controller.cameraSelector =
+                                            if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                                                CameraSelector.DEFAULT_FRONT_CAMERA
+                                            } else
+                                                CameraSelector.DEFAULT_BACK_CAMERA
+                                              },
+                                    modifier= Modifier.size(54.dp),
+                                    shape = CircleShape,
+                                    border= BorderStroke(1.dp, Color.White),
+                                    contentPadding = PaddingValues(8.dp),
+                                    colors = ButtonDefaults.outlinedButtonColors(contentColor =  Color.White)
+                                ) {
+                                    Icon(Icons.Default.Cached, contentDescription = "Swap camera", tint = Color.White, modifier = Modifier.size(54.dp))
+                                }
+                            }
                         }
                     }
                 }
